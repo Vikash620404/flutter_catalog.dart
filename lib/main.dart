a53c9981256b89cab73610b19bbe5c04840493a1
vikash kumar <vikashkumar620404800@gmail.com>
2022-03-12 02:16:44 +0530

    Day-1 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_catalog/home_page.dart';
+
 
 void main() {
   runApp(MyApp());
@@ -10,10 +10,12 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    var day = 5;
+    //var day = 5;
 
     return MaterialApp(
-      home: Homepage(),
+     home: Container(
+     child:Text("Welcome to 30 days flutter"),
+     )
     );
   }
 }
